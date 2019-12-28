565bf7dc287ba319c4b864b5bdf97670e135b04d
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-12-28 15:35:55 +0800

    [neto/assets/*.sh] Added downloader for external assets(sounds/images).

---
+++ neto/assets/download-external-assets-images.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mkdir -p images && cd images
+
+# Place your corresponding assets the right location as stated to make the application work properly.
+# @see https://stackoverflow.com/questions/4944295/skip-download-if-files-exist-in-wget
+# The corresponding licenses should be respected; use the images and audios at your own risk.
+
+# Globally icons needed.
+wget -N -O icon.png https://www.iconfinder.com/icons/1553066/download/png/128
+wget -N -O traveler.png https://www.iconfinder.com/icons/4698587/download/png/128
+
+# Personal Applications
+wget -N -O workspace.png https://cdn2.iconfinder.com/data/icons/free-version/128/workplace-128.png
+wget -N -O schedules.png https://www.iconfinder.com/icons/5027858/download/png/128
+wget -N -O noting.png https://www.iconfinder.com/icons/1296370/download/png/128
+
+# Campus Applications
+wget -N -O activities.png https://cdn2.iconfinder.com/data/icons/free-version/128/baseball-128.png
+wget -N -O satelite.png https://cdn2.iconfinder.com/data/icons/free-version/128/satelite-128.png
+cp satelite.png marketplace.png
+wget -N -O calendar.png https://www.iconfinder.com/icons/1296363/download/png/128
+wget -N -O news.png https://www.iconfinder.com/icons/221797/download/png/128
+
+# Infra Tools
+wget -N -O text-tailor.png https://www.iconfinder.com/icons/3213307/download/png/128
--- neto/assets/download-external-assets.sh
+++ neto/assets/download-external-assets.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-mkdir -p images sounds
+mkdir -p sounds && cd sounds
 
-# Place your corresponding assets the right location as stated to make the application work.
+# Place your corresponding assets the right location as stated to make the application work properly.
 # @see https://stackoverflow.com/questions/4944295/skip-download-if-files-exist-in-wget
 # The corresponding licenses should be respected; use the images and audios at your own risk.
-wget -N -O images/icon.png https://www.iconfinder.com/icons/1553066/download/png/128
-wget -N -O sounds/noti-regular.mp3 https://notificationsounds.com/notification-sounds/clearly-602/download/mp3
-wget -N -O sounds/noti-general.mp3 https://notificationsounds.com/message-tones/open-up-587/download/mp3
-wget -N -O sounds/noti-important.mp3 https://notificationsounds.com/notification-sounds/eventually-590/download/mp3
-wget -N -O sounds/noti-warning.mp3 https://notificationsounds.com/notification-sounds/inflicted-601/download/mp3
-wget -N -O sounds/noti-error.mp3 https://notificationsounds.com/animals/bullfrog-477/download/mp3
+wget -N -O noti-regular.mp3 https://notificationsounds.com/notification-sounds/clearly-602/download/mp3
+wget -N -O noti-general.mp3 https://notificationsounds.com/message-tones/open-up-587/download/mp3
+wget -N -O noti-important.mp3 https://notificationsounds.com/notification-sounds/eventually-590/download/mp3
+wget -N -O noti-warning.mp3 https://notificationsounds.com/notification-sounds/inflicted-601/download/mp3
+wget -N -O noti-error.mp3 https://notificationsounds.com/animals/bullfrog-477/download/mp3
