cf39347259e918d66e3cf47da5fcde0fe524157b
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-12-28 19:51:20 +0800

    [prepare-to-deploy.sh] Updated the script to archive the current builds.

--- prepare-to-deploy.sh
+++ prepare-to-deploy.sh
@@ -5,10 +5,14 @@
 TERMINATING_TIME=`date +%Y-%m-%dT%H_%M_%S`;
 DIR_BUILDS="builds/Builds_${TERMINATING_TIME}"
 
-mkdir -p $DIR_BUILDS
 DIR_DIST="dist"
+ls -al ${DIR_DIST} && ls -al -h ${DIR_DIST}
+md5sum ${DIR_DIST}/vendors.chunk.js ${DIR_DIST}/infraTextTailor.bundle.js
 
+set -o xtrace
+mkdir -p $DIR_BUILDS
 mkdir ${DIR_BUILDS}/Text-Tailor
+cp ${DIR_DIST}/vendors.chunk.js ${DIR_BUILDS}/Text-Tailor
 cp ${DIR_DIST}/infraTextTailor.bundle.js ${DIR_BUILDS}/Text-Tailor
 
 tree -h ${DIR_BUILDS}
