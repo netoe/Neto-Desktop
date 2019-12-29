ff9c8764e5c8ec5a22f265a08350a6fe2ad58ba9
Yu Zhanbei <awesome.zhanbei@gmail.com>
2019-12-29 22:44:47 +0800

    [src/graphic|App] Supported multiple languages; added #Neto-Dashboards.

---
+++ neto/built/AppDashboards.chunk.js
@@ -0,0 +1,1 @@
+../../dist/AppDashboards.chunk.js
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 'use strict';
 
 import React from 'react';
+import {ContextLanguageProvider, DEFAULT_LANGUAGE_CONTEXT_VALUE} from 'src/graphic/mui-lib/hooks/useLanguage';
 import {AppHome} from './graphic/AppHome/AppHome';
 
 // App entrance: a single page without router.
 export const App = React.memo(() => {
+	const [language, setLanguage] = React.useState(DEFAULT_LANGUAGE_CONTEXT_VALUE.language as string);
 	return (
-		<AppHome/>
+		<ContextLanguageProvider value={{language, languages: DEFAULT_LANGUAGE_CONTEXT_VALUE.languages}}>
+			<AppHome onSetLanguage={setLanguage}/>
+		</ContextLanguageProvider>
 	);
 });
