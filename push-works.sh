b174c6ee3c9255b720fcb5e2056c4dbd4e531296
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-01-02 18:12:22 +0800

    [push-works.sh] Added script to push local commits to remote origin.

---
+++ push-works.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git push origin master
+
+cd src/demo
+git push origin master
+cd -
+
+cd src/demo/Neto-Noting
+git push origin master
+cd -
+
+cd src/graphic
+git push origin master
+cd -
+
+cd src/graphic/Neto-Dashboards
+git push origin master
+cd -
