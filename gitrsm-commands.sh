78cb7e0a9e1680b88aac17d97dd9b1c510ab8f66
Yu Zhanbei <awesome.zhanbei@gmail.com>
2020-01-05 22:05:19 +0800

    [gitrsm-commands] Enriched submodule cmd to push works and get statuses.

--- gitrsm-commands.sh
+++ gitrsm-commands.sh
@@ -2,12 +2,16 @@
 
 
 # gitrsm = Git Recursive SubModules.
-alias gitrsm-status='git submodule status --recursive'
-alias gitrsm-remote-status='git submodule foreach --recursive "git status | grep --color=always \"ahead.*commits*.\" || true && echo"'
+alias gitrsm-simplified-status='git submodule status --recursive'
 alias gitrsm-detailed-status='git submodule foreach --recursive git status'
+alias gitrsm-local-status='git submodule foreach --recursive "git status | grep --color=always \"(new commits\|modified content)\" || true && echo"'
+alias gitrsm-remote-status='git submodule foreach --recursive "git status | grep --color=always \"ahead.*commits*.\" || true && echo"'
+alias gitrsm-status='git submodule foreach --recursive "git status | grep --color=always \"ahead.*commits*.\|(new commits\|modified content)\" || true && echo"'
 
-alias gitrsm-push='git submodule foreach --recursive git push origin master'
 alias gitrsm-pull='git submodule foreach --recursive git pull origin master'
+alias gitrsm-raw-push='git submodule foreach --recursive git push origin master'
+cmdGitPushIfNeeded='git status | grep ahead && echo Pushing from ${toplevel}/${name} && git push origin master || true'
+alias gitrsm-push-works="git submodule foreach --recursive '${cmdGitPushIfNeeded}'"
 alias gitrsm-checkout-master='git submodule foreach --recursive git checkout master'
 alias gitrsm-fetch='git submodule foreach --recursive git fetch origin master'
 alias gitrsm-merge-origin-master='git submodule foreach --recursive git merge origin/master'
